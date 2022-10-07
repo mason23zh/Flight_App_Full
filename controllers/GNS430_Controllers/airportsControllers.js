ea14e25a040ef88231e29b3d21ab90e516815dd0
Zhengyang Zhao <mason.zh@gmail.com>
2022-10-06 21:44:51 -0500

    refactor GNS430 airport model with name from global airport dataset. refactor getAirportByName_GSN430 controller

--- controllers/GNS430_Controllers/airportsControllers.js
+++ controllers/GNS430_Controllers/airportsControllers.js
@@ -7,7 +7,13 @@ const { generateResponseMetar } = require("../../utils/METAR/generateResponseMET
 const { generateResponseATIS } = require("../../utils/ATIS/generateResponseATIS");
 
 module.exports.getAirportByICAO_GNS430 = async (req, res, next) => {
-    const gns430Airport = await GNS430Airport.findOne({ ICAO: `${req.params.icao.toUpperCase()}` });
+    const airportFeatures = new APIFeatures(
+        GNS430Airport.findOne({ ICAO: `${req.params.icao.toUpperCase()}` }),
+        req.query
+    ).limitFields();
+
+    const gns430Airport = await airportFeatures.query;
+
     if (!gns430Airport) {
         throw new BadRequestError(`Airport with ICAO ${req.params.icao.toUpperCase()} not found. `);
     }
@@ -40,7 +46,12 @@ module.exports.getAirportByIATA_GNS430 = async (req, res, next) => {
     }
 
     const airportICAO_Code = airportICAO[0].ident;
-    const gns430Airport = await GNS430Airport.find({ ICAO: airportICAO_Code });
+    //const gns430Airport = await GNS430Airport.find({ ICAO: airportICAO_Code });
+
+    const airportFeatures = new APIFeatures(GNS430Airport.find({ ICAO: airportICAO_Code }), req.query).limitFields();
+
+    const gns430Airport = await airportFeatures.query;
+
     if (!gns430Airport) {
         throw new NotFoundError(`Can Not Found Airport with IATA: ${req.params.iata.toUpperCase()}`);
     }
@@ -61,43 +72,19 @@ module.exports.getAirportByIATA_GNS430 = async (req, res, next) => {
 
 //TODO: If search name returns too many results, this will take forever to finish. Need optimize DB model.
 module.exports.getAirportByName_GNS430 = async (req, res) => {
-    // Since Airports data from global airport dataset has more complete name
-    // Search name in global airports first and return ICAO to be used in the
-    // GNS430 data.
-    // GNS430 data is less complete than global airports dataset, so if ICAO
-    // does not include in the GNS430 data, return global airports dataset.
-
-    // Get airport using the name from the global airport dataset
-    const airportsQueryObj = await Airports.find({
+    const airportQueryObj = GNS430Airport.find({
         name: { $regex: `${req.params.name}`, $options: "i" },
     });
 
-    if (airportsQueryObj.length === 0) {
-        throw new NotFoundError(`Airports with name '${req.params.name}' could not be found.`);
-    }
+    const featuresQuery = new APIFeatures(airportQueryObj, req.query).filter().limitFields().limitResults().paginate();
 
-    // Extract ICAO from the found global airport dataset
-    const aviableAirportsICAOfromGlobal = airportsQueryObj.map((airport) => airport.ident);
-
-    // Build array of promises (GNS430 Airport) based on the found global airport
-    const airportPromises = [];
-    for (const icao of aviableAirportsICAOfromGlobal) {
-        airportPromises.push(await GNS430Airport.findOne({ ICAO: icao }));
-    }
-
-    //Resolve promises
-    const gns430AirportsData = await Promise.allSettled(airportPromises);
-    const responseGNSAirport = gns430AirportsData.filter((result) => {
-        if (result.status === "fulfilled" && result.value !== null) {
-            return result.value;
-        }
-    });
+    const airports = await featuresQuery.query;
 
     res.status(200).json({
         status: "success",
-        results: responseGNSAirport.length,
+        results: airports.length,
         data: {
-            airport: responseGNSAirport,
+            airport: airports,
         },
     });
 };
