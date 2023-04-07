e67447b7df7629e56622f9f7044bf1b152dd66d8
Zhengyang Zhao <mason.zh@gmail.com>
2023-04-06 23:24:03 -0500

    ExtremeWeather page header layout

--- client/src/component/Dropdown.jsx
+++ client/src/component/Dropdown.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState, useRef } from "react";
+import classNames from "classnames";
 import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 import Panel from "./Panel";
 
-const Dropdown = ({ options, onChange, value }) => {
+const Dropdown = ({ options, onChange, value, className }) => {
+    const dropDownFinalClassName = classNames("w-48 flex flex-col", className);
+
     const [isOpen, setIsOpen] = useState(false);
     // get reference to root element of Dropdown
     const divElement = useRef();
@@ -38,7 +41,7 @@ const Dropdown = ({ options, onChange, value }) => {
     const renderedOptions = options.map((option) => {
         return (
             <div
-                className="hover:bg-sky-100 rounded cursor-pointer p-1"
+                className="hover:bg-sky-200 rounded cursor-pointer p-1 duration-50"
                 onClick={() => handleOptionClick(option)}
                 key={option.code}
             >
@@ -47,8 +50,8 @@ const Dropdown = ({ options, onChange, value }) => {
         );
     });
     return (
-        <div ref={divElement} className="w-48 flex flex-col absolute top-[5%]">
-            <Panel className="flex justify-between items-center cursor-pointer" onClick={handleClick}>
+        <div ref={divElement} className={dropDownFinalClassName}>
+            <Panel className="flex justify-between items-center cursor-pointer " onClick={handleClick}>
                 {value?.name || "Select..."}
                 {isOpen ? <GoChevronDown className="text-lg" /> : <GoChevronLeft className="text-lg" />}
             </Panel>
