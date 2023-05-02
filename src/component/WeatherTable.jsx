ecd6d823eab96f2a6b18139de0d20557a7a52819
Zhengyang Zhao <mason.zh@gmail.com>
2023-05-02 17:18:15 -0500

    added button to add more data or set to default

--- src/component/WeatherTable.jsx
+++ src/component/WeatherTable.jsx
@@ -1,12 +1,11 @@
 // noinspection JSUnusedGlobalSymbols
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
     CgChevronDoubleDown, CgChevronDoubleUp,
 } from "react-icons/cg";
 import { useTable, useExpanded } from "react-table";
 import { useSelector } from "react-redux";
-import axios from "axios";
 import {
     BARO,
     TEMPERATURE, VISIBILITY, WIND_GUST, WIND_SPEED,
@@ -18,9 +17,10 @@ const INHG_TO_HPA = 33.863886666667;
 
 function WeatherTable({ expandedContent }) {
     let columnsToRender;
-    let requestParams = { limit: 10 };
     const [sortOrder, setSortOrder] = useState(1);
     const [rowData, setRowData] = useState(null);
+    const [requestLimit, setRequestLimit] = useState(10);
+    let requestParams = { limit: requestLimit };
     const { weather, scope, code } = useSelector((state) => state.extremeWeather.userSelection);
     if (weather === TEMPERATURE || weather === BARO) {
         requestParams = { ...requestParams, sort: sortOrder };
@@ -175,91 +175,115 @@ function WeatherTable({ expandedContent }) {
         </th>
     );
     
+    const handleLoadMoreData = () => {
+        setRequestLimit(requestLimit + 10);
+    };
+    
+    const handleSetDataToDefault = () => {
+        setRequestLimit(10);
+    };
+    
     
     return (
-        <table
-            {...getTableProps()}
-        >
-            <thead>
-                {headerGroups.map((headerGroup) => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map((column) => {
-                            if (column.Header === "Baro") {
-                                return (
-                                    <th
-                                        {...column.getHeaderProps()}
-                                        className="text-xl p-2 border-5 bg-red-300"
-                                        onClick={handleSortClick}
-                                    >
-                                        <div className="flex items-center justify-center hover:cursor-pointer">
-                                            {column.render("Header")}
-                                            {sortOrder === -1 ? <CgChevronDoubleDown /> : <CgChevronDoubleUp />}
-                                        </div>
-                                    </th>
-                                );
-                            }
-                        
-                            if (column.Header === "Temperature") {
-                                return (
-                                    <th
-                                        {...column.getHeaderProps()}
-                                        className="text-xl p-2 border-5 bg-red-300"
-                                        onClick={handleSortClick}
-                                    >
-                                        <div className="flex items-center justify-center hover:cursor-pointer">
-                                            {column.render("Header")}
-                                            {sortOrder === -1 ? <CgChevronDoubleDown /> : <CgChevronDoubleUp />}
-                                        </div>
-                                    </th>
-                                );
-                            }
-                        
-                            return (
-                                <th
-                                    {...column.getHeaderProps()}
-                                    className="text-xl p-2 bg-red-300"
-                                >
-                                    {column.render("Header")}
-                                </th>
-                            );
-                        })}
-                    </tr>
-                ))}
-            </thead>
-            <tbody
-                {...getTableBodyProps()}
-                className="[&>*:nth-child(odd)]:bg-blue-300 [&>*:nth-child(even)]:bg-orange-200"
+        <div className="flex flex-col justify-center items-center gap-2">
+            <table
+                {...getTableProps()}
             >
-                {rows.map((row) => {
-                    prepareRow(row);
-                    const rowProps = row.getRowProps();
-                    delete rowProps.role;
-                    return (
-                        <React.Fragment key={rowProps.key} {...rowProps}>
-                            <tr
-                                className=" text-xl text-center"
-                            >
-                                {row.cells.map((cell) => (
-                                    <td
-                                        {...cell.getCellProps()}
-                                        className="p-5"
+                <thead>
+                    {headerGroups.map((headerGroup) => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map((column) => {
+                                if (column.Header === "Baro") {
+                                    return (
+                                        <th
+                                            {...column.getHeaderProps()}
+                                            className="text-xl p-2 border-5 bg-red-300"
+                                            onClick={handleSortClick}
+                                        >
+                                            <div className="flex items-center justify-center hover:cursor-pointer">
+                                                {column.render("Header")}
+                                                {sortOrder === -1 ? <CgChevronDoubleDown /> : <CgChevronDoubleUp />}
+                                            </div>
+                                        </th>
+                                    );
+                                }
+                            
+                                if (column.Header === "Temperature") {
+                                    return (
+                                        <th
+                                            {...column.getHeaderProps()}
+                                            className="text-xl p-2 border-5 bg-red-300"
+                                            onClick={handleSortClick}
+                                        >
+                                            <div className="flex items-center justify-center hover:cursor-pointer">
+                                                {column.render("Header")}
+                                                {sortOrder === -1 ? <CgChevronDoubleDown /> : <CgChevronDoubleUp />}
+                                            </div>
+                                        </th>
+                                    );
+                                }
+                            
+                                return (
+                                    <th
+                                        {...column.getHeaderProps()}
+                                        className="text-xl p-2 bg-red-300"
                                     >
-                                        {cell.render("Cell")}
-                                    </td>
-                                ))}
-                            </tr>
-                            {row.isExpanded ? (
-                                <tr>
-                                    <td colSpan={visibleColumns.length}>
-                                        {expandedContent({ row })}
-                                    </td>
+                                        {column.render("Header")}
+                                    </th>
+                                );
+                            })}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody
+                    {...getTableBodyProps()}
+                    className="[&>*:nth-child(odd)]:bg-blue-300 [&>*:nth-child(even)]:bg-orange-200"
+                >
+                    {rows.map((row) => {
+                        prepareRow(row);
+                        const rowProps = row.getRowProps();
+                        delete rowProps.role;
+                        return (
+                            <React.Fragment key={rowProps.key} {...rowProps}>
+                                <tr
+                                    className=" text-xl text-center"
+                                >
+                                    {row.cells.map((cell) => (
+                                        <td
+                                            {...cell.getCellProps()}
+                                            className="p-5"
+                                        >
+                                            {cell.render("Cell")}
+                                        </td>
+                                    ))}
                                 </tr>
-                            ) : null}
-                        </React.Fragment>
-                    );
-                })}
-            </tbody>
-        </table>
+                                {row.isExpanded ? (
+                                    <tr>
+                                        <td colSpan={visibleColumns.length}>
+                                            {expandedContent({ row })}
+                                        </td>
+                                    </tr>
+                                ) : null}
+                            </React.Fragment>
+                        );
+                    })}
+                </tbody>
+            </table>
+            <div className="flex justify-center gap-3">
+                <button
+                    onClick={handleLoadMoreData}
+                    className="px-2 py-1 bg-green-400 rounded-xl text-lg hover:bg-amber-400 shadow-2xl"
+                >
+                    Load More...
+                </button>
+                <button
+                    onClick={handleSetDataToDefault}
+                    className="px-2 py-1 bg-red-400 rounded-xl text-lg hover:bg-amber-400 shadow-2xl "
+                >
+                    Set Default
+                </button>
+            </div>
+        </div>
     );
 }
 
