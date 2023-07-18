ba6486e36d742d6d95f1ece9382d49386f079855
Zhengyang Zhao <mason.zh@gmail.com>
2023-07-17 20:12:56 -0500

    added map to AirportDetail.jsx

--- src/component/AirportDetail.jsx
+++ src/component/AirportDetail.jsx
@@ -4,6 +4,10 @@
  * */
 import React, { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
+import {
+    MapContainer, Marker, Popup, TileLayer, useMap,
+} from "react-leaflet";
+
 
 function AirportDetail() {
     const [airport, setAirport] = useState();
@@ -17,13 +21,34 @@ function AirportDetail() {
     }, []);
     
     if (airport) {
+        console.log(airport);
+        const [lng, lat] = airport.station.geometry.coordinates;
         return (
-            <div>
-                Name: {airport.station.name}
-                ICAO: {airport.ICAO}
+            <div className="flex flex-col items-center">
+                <div>
+                    Name: {airport.station.name}
+                    ICAO: {airport.ICAO}
+                </div>
+                <div>
+                    <MapContainer
+                        center={[lat, lng]}
+                        zoom={13}
+                        scrollWheelZoom={false}
+                    >
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                    </MapContainer>
+                </div>
             </div>
         );
     }
+    return (
+        <div>
+            Loading...
+        </div>
+    );
 }
 
 export default AirportDetail;
