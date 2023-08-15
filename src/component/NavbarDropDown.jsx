3cc16b355ce4b46c17be0f7f8b2b73b5b4cf40b8
Zhengyang Zhao <mason.zh@gmail.com>
2023-08-15 17:13:59 -0500

    fix the forward ref issue in the NavbarDropDown

--- src/component/NavbarDropDown.jsx
+++ src/component/NavbarDropDown.jsx
@@ -1,29 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Dropdown from "rsuite/Dropdown";
 import { Link } from "react-router-dom";
 import { IoListOutline, IoMoon, IoSunnyOutline } from "react-icons/io5";
+import { Nav } from "rsuite";
 import { useTheme, useThemeUpdate } from "../hooks/ThemeContext";
 
 function NavbarDropDown() {
     const toggleTheme = useThemeUpdate();
     const darkMode = useTheme();
+    
+    const NavLink = React.forwardRef(({ href, children, ...rest }, ref) => (
+        <Link ref={ref} to={href} {...rest}>
+            {children}
+        </Link>
+    ));
+    
     return (
         <div>
-            <Dropdown title={<IoListOutline />} noCaret>
-                <Dropdown.Item>
-                    <Link style={{ textDecoration: "none" }} to="/airport">Airport</Link>
+            <Dropdown
+                title={<IoListOutline size={25} />}
+                noCaret
+            >
+                <Dropdown.Item as={NavLink} href="/airport">Airport</Dropdown.Item>
+                <Dropdown.Item as={NavLink} href="/weather">Weather</Dropdown.Item>
+                <Dropdown.Item as={NavLink} href="/extreme-weather" className="text-red-400">Extreme
+                    weather
                 </Dropdown.Item>
-                <Dropdown.Item>
-                    <Link style={{ textDecoration: "none" }} to="/weather">Weather</Link>
-                </Dropdown.Item>
-                <Dropdown.Item>
-                    <Link style={{ textDecoration: "none" }} to="/extreme-weather" className="text-red-400">
-                        Extreme weather
-                    </Link>
-                </Dropdown.Item>
-                <Dropdown.Item>
-                    <Link style={{ textDecoration: "none" }} to="/about">About</Link>
-                </Dropdown.Item>
+                <Dropdown.Item as={NavLink} href="/about">About</Dropdown.Item>
+                <Dropdown.Item divider />
                 <Dropdown.Item>
                     <div
                         className="flex flex-row justify-center items-center gap-4"
