ded7466d32c54076d260f0cbb473bc74a6f5ad7d
Zhengyang Zhao <mason.zh@gmail.com>
2023-09-18 21:45:46 -0500

    Replace Airport Detail Table with Airport Detail Panel

--- src/component/AirportDetail.jsx
+++ src/component/AirportDetail.jsx
@@ -146,23 +146,23 @@ function AirportDetail() {
                             <AirportMap lat={lat} lng={lng} name={name} />
                         </div>
                     </div>
-                    <div className="mt-3 p-2 w-[80%] justify-self-center">
-                        <AirportDetailTable
-                            ICAO={ICAO}
-                            iata={iata}
-                            region={region_name}
-                            country={country_name}
-                            runwayCount={airport.runways.length}
-                            airportType={type}
-                            elevation={elevation}
-                            transitionAltitude={transitionAltitude}
-                            lng={lng}
-                            lat={lat}
-                            homeLink={home_link}
-                            wikiLink={wikipedia_link}
-                        />
-                    </div>
-                    <div className="mt-3 max-w-4xl ml-2 mr-2 p-2 justify-self-center text-center md:ml-0 md:mr-0">
+                    {/* <div className="mt-3 p-2 w-[80%] justify-self-center"> */}
+                    {/*    <AirportDetailTable */}
+                    {/*        ICAO={ICAO} */}
+                    {/*        iata={iata} */}
+                    {/*        region={region_name} */}
+                    {/*        country={country_name} */}
+                    {/*        runwayCount={airport.runways.length} */}
+                    {/*        airportType={type} */}
+                    {/*        elevation={elevation} */}
+                    {/*        transitionAltitude={transitionAltitude} */}
+                    {/*        lng={lng} */}
+                    {/*        lat={lat} */}
+                    {/*        homeLink={home_link} */}
+                    {/*        wikiLink={wikipedia_link} */}
+                    {/*    /> */}
+                    {/* </div> */}
+                    <div className="mt-3 w-[100%] md:w-[70%] ml-2 mr-2 p-2 justify-self-center text-center md:ml-0 md:mr-0">
                         <div className="w-auto">
                             <AirportDetailPanel
                                 ICAO={ICAO}
@@ -180,7 +180,7 @@ function AirportDetail() {
                             />
                         </div>
                     </div>
-                    <div className="mt-3 p-2 max-w-[1230px] w-[84%] justify-self-center">
+                    <div className="mt-3 p-2 max-w-[1230px] w-[90%] justify-self-center">
                         <AirportDetailRunwayTable runways={airport.runways} metar={metar} />
                     </div>
                     <div className="ml-3 mr-3 p-2 justify-self-center">{renderWidget()}</div>
--- src/component/AirportDetailPanel.jsx
+++ src/component/AirportDetailPanel.jsx
@@ -21,9 +21,10 @@ function AirportDetailPanel({
         </div>
     );
     
+    
     return (
         <Panel header={`${ICAO} Detail Information`} collapsible bordered defaultExpanded>
-            <div className="grid gird-cols-2 text-sm sm:text-lg">
+            <div className="grid gird-cols-2 gap-2 text-sm sm:text-lg">
                 <div className="hover:border rounded-xl">
                     <div className="grid grid-cols-2 ">
                         <div className="text-left sm:text-center">ICAO</div>
@@ -109,9 +110,13 @@ function AirportDetailPanel({
                     <div className="grid grid-cols-2 ">
                         <div className="text-left sm:text-center">Home Page</div>
                         <div className="text-right sm:text-center">
-                            <a href={homeLink} target="_blank" rel="noreferrer">
-                                {ICAO} Home Page
-                            </a>
+                            {homeLink
+                                ? (
+                                    <a href={homeLink} target="_blank" rel="noreferrer">
+                                        {ICAO} Home Page
+                                    </a>
+                                )
+                                : "N/A"}
                         </div>
                     </div>
                 </div>
@@ -120,9 +125,12 @@ function AirportDetailPanel({
                     <div className="grid grid-cols-2 ">
                         <div className="text-left sm:text-center">Wiki Page</div>
                         <div className="text-right sm:text-center">
-                            <a href={wikiLink} target="_blank" rel="noreferrer">
-                                {ICAO} Wiki Page
-                            </a>
+                            {wikiLink
+                                ? (
+                                    <a href={wikiLink} target="_blank" rel="noreferrer">
+                                        {ICAO} Wiki Page
+                                    </a>
+                                ) : "N/A"}
                         </div>
                     </div>
                 </div>
