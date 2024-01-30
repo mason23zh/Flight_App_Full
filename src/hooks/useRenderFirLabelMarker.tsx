65da2cff033efc8323c48edf9497a9ca975d1889
Zhengyang Zhao <mason.zh@gmail.com>
2024-01-30 16:58:42 -0600

    added debounce on both tracon label hover and fir label hover

--- src/hooks/useRenderFirLabelMarker.tsx
+++ src/hooks/useRenderFirLabelMarker.tsx
@@ -1,23 +1,49 @@
 import GeoJson from "geojson";
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Marker } from "react-map-gl";
 
 const useRenderFirLabelMarker = (geoJsonFeatures: GeoJson.FeatureCollection) => {
     const [hoverFir, setHoverFir] = useState<GeoJson.FeatureCollection>(null);
+    const [hoverDelayHandler, setHoverDelayHandler] = useState(null);
     let renderedMarkers;
 
-    const handleOnMouseOver = (feature: GeoJson.Feature) => {
-        console.log("Mouse enter");
-        console.log("FirLabel geojson feature", feature);
-        setHoverFir({
-            type: "FeatureCollection",
-            features: [feature]
-        });
-    };
+    const handleOnMouseOver = useCallback((feature: GeoJson.Feature) => {
+        if (hoverDelayHandler) {
+            clearTimeout(hoverDelayHandler);
+        }
 
-    const handleOnMouseLeave = () => {
-        setHoverFir(null);
-    };
+        const handler = setTimeout(() => {
+            setHoverFir({
+                type: "FeatureCollection",
+                features: [feature]
+            });
+        }, 150);
+
+        setHoverDelayHandler(handler);
+    }, [hoverDelayHandler]);
+
+    const handleOnMouseLeave = useCallback(() => {
+        // Clear the timeout when the mouse leaves.
+        if (hoverDelayHandler) {
+            clearTimeout(hoverDelayHandler);
+        }
+
+        const handler = setTimeout(() => {
+            setHoverFir(null);
+        }, 150);
+
+        setHoverDelayHandler(handler);
+    }, [hoverDelayHandler]);
+
+    // Effect to clear any timeouts when the component unmounts.
+    useEffect(() => {
+        return () => {
+            if (hoverDelayHandler) {
+                clearTimeout(hoverDelayHandler);
+            }
+        };
+    }, [hoverDelayHandler]);
+
 
     if (geoJsonFeatures) {
         renderedMarkers = geoJsonFeatures.features.map((feature) => {
--- src/hooks/useRenderTraconLabelMarker.tsx
+++ src/hooks/useRenderTraconLabelMarker.tsx
@@ -1,21 +1,63 @@
 import GeoJson from "geojson";
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { Marker } from "react-map-gl";
 
 const useRenderTraconLabelMarker = (geoJsonFeatures: GeoJson.FeatureCollection) => {
     const [hoverTracon, setHoverTracon] = useState<GeoJson.FeatureCollection>(null);
+    const [hoverDelayHandler, setHoverDelayHandler] = useState(null);
+
     console.log("use render tracon label");
 
-    const handleOnMouseOver = (feature: GeoJson.Feature) => {
-        setHoverTracon({
-            type: "FeatureCollection",
-            features: [feature]
-        });
-    };
+    // const handleOnMouseOver = (feature: GeoJson.Feature) => {
+    //     setHoverTracon({
+    //         type: "FeatureCollection",
+    //         features: [feature]
+    //     });
+    // };
+    //
+    // const handOnMouseLeave = () => {
+    //     setHoverTracon(null);
+    // };
 
-    const handOnMouseLeave = () => {
-        setHoverTracon(null);
-    };
+
+    const handleOnMouseOver = useCallback((feature: GeoJson.Feature) => {
+        if (hoverDelayHandler) {
+            clearTimeout(hoverDelayHandler);
+        }
+
+        const handler = setTimeout(() => {
+            setHoverTracon({
+                type: "FeatureCollection",
+                features: [feature]
+            });
+        }, 150);
+
+        setHoverDelayHandler(handler);
+    }, [hoverDelayHandler]);
+
+    const handOnMouseLeave = useCallback(() => {
+        // Clear the timeout when the mouse leaves.
+        if (hoverDelayHandler) {
+            clearTimeout(hoverDelayHandler);
+        }
+
+        const handler = setTimeout(() => {
+            setHoverTracon(null);
+        }, 150);
+
+        setHoverDelayHandler(handler);
+    }, [hoverDelayHandler]);
+
+    // Effect to clear any timeouts when the component unmounts.
+    useEffect(() => {
+        return () => {
+            if (hoverDelayHandler) {
+                clearTimeout(hoverDelayHandler);
+            }
+        };
+    }, [hoverDelayHandler]);
+
+
     const renderMarkers = (geoJsonFeatures: GeoJson.FeatureCollection) => {
         console.log("Render marker function run.");
         if (geoJsonFeatures) {
