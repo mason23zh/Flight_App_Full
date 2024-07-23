b9da3972b8ef2c966dd02194ea514bb5d477d1cc
Zhengyang Zhao <mason.zh@gmail.com>
2024-07-23 05:51:06 -0500

    update aircraft.json; added more style to SearchBoxAircraftGroup

--- src/component/2d/map_feature_toggle_button/search_box/SearchBoxAircraftGroup.tsx
+++ src/component/2d/map_feature_toggle_button/search_box/SearchBoxAircraftGroup.tsx
@@ -8,8 +8,17 @@ interface Props {
 const SearchBoxAircraftGroup = ({ aircraft }: Props) => {
     console.log("Aircraft by type:", aircraft);
     return (
-        <div>
-            {aircraft.aircraftType}
+        <div className="grid grid-cols-4 items-center w-full border-b p-1">
+            <div className="col-span-1">
+                {aircraft.aircraftType}
+            </div>
+            <div className="col-span-2">
+                {aircraft.flights[0].flight_plan?.aircraft_name || "-"}
+            </div>
+            <div className="flex gap-1 col-span-1">
+                <div>{aircraft.flights.length}</div>
+                <div>Online</div>
+            </div>
         </div>
     );
 };
