198fa1a278e4fff907a55a6ad5431eda4df16c12
Zhengyang Zhao <mason.zh@gmail.com>
2024-07-23 06:00:38 -0500

    adjust style in SearchBoxAircraftGroup

--- src/component/2d/map_feature_toggle_button/search_box/SearchBoxAircraftGroup.tsx
+++ src/component/2d/map_feature_toggle_button/search_box/SearchBoxAircraftGroup.tsx
@@ -8,7 +8,8 @@ interface Props {
 const SearchBoxAircraftGroup = ({ aircraft }: Props) => {
     console.log("Aircraft by type:", aircraft);
     return (
-        <div className="grid grid-cols-4 items-center w-full border-b p-1">
+        <div className="font-Rubik grid grid-cols-4 gap-2
+        items-center w-full border-b p-1 hover:bg-gray-400 hover:cursor-pointer">
             <div className="col-span-1">
                 {aircraft.aircraftType}
             </div>
@@ -16,8 +17,8 @@ const SearchBoxAircraftGroup = ({ aircraft }: Props) => {
                 {aircraft.flights[0].flight_plan?.aircraft_name || "-"}
             </div>
             <div className="flex gap-1 col-span-1">
+                <div>Online:</div>
                 <div>{aircraft.flights.length}</div>
-                <div>Online</div>
             </div>
         </div>
     );
