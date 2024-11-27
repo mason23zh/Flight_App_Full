76ab38d1e71f3bd1a11bf72616ea71082e7e782c
Zhengyang Zhao <mason.zh@gmail.com>
2024-11-26 23:17:45 -0600

    make VirtuosoScroller to accept custom styled scroll bar

--- src/util/VirtuosoScroller.tsx
+++ src/util/VirtuosoScroller.tsx
@@ -1,14 +1,22 @@
 import React, { forwardRef } from "react";
+
 /*
 * A custom scroll bar that used in the Virtuoso list
 * */
-//TODO: make className into props so the scrollbar style can be changed.
-const Scroller = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>((props, ref) => {
+
+interface ScrollerProps extends React.HTMLAttributes<HTMLDivElement> {
+    className?: string;
+}
+
+const Scroller = forwardRef<HTMLDivElement, ScrollerProps>(({
+    className,
+    ...props
+}, ref) => {
     return (
         <div
             {...props}
             ref={ref}
-            className="scrollbar scrollbar-thin scrollbar-thumb-slate-700 scrollbar-track-gray-500"
+            className={className || "scrollbar scrollbar-thin scrollbar-thumb-slate-700 scrollbar-track-gray-500"}
         />
     );
 });
