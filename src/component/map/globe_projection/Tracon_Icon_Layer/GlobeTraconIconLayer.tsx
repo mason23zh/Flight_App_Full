d578189ed06f8c8426b5f40d22a57ceee1ea8344
Zhengyang Zhao <mason.zh@gmail.com>
2025-02-01 01:09:03 -0600

    use setData for GlobeTraconIconLayer

--- src/component/map/globe_projection/Tracon_Icon_Layer/GlobeTraconIconLayer.tsx
+++ src/component/map/globe_projection/Tracon_Icon_Layer/GlobeTraconIconLayer.tsx
@@ -1,14 +1,23 @@
-import React, { useEffect, useMemo } from "react";
+import React, { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../../store";
-import { Layer, Source, useMap } from "react-map-gl";
+import { GeoJSONSource, Layer, Source, useMap } from "react-map-gl";
 import { FallbackTracon, MatchedTracon } from "../../../../hooks/useMatchTracon";
 import generateTraconIcon from "../../mapbox_Layer/util/generateTraconIcon";
-import { GeoJSON } from "geojson";
+import _ from "lodash";
 
+interface TraconFeature {
+    uniqueId: string;
+    coordinates: number[];
+    iconId: string;
+    isFallback: boolean;
+    originalData: MatchedTracon | FallbackTracon;
+}
 
+//TODO: Optimize the image loading
 const GlobeTraconIconLayer = () => {
     const { current: mapRef } = useMap();
+    const previousTraconsRef = useRef<TraconFeature[]>([]);
     const imagePrefix = "tracon-icon-";
     const {
         matchedFallbackTracons,
@@ -17,155 +26,152 @@ const GlobeTraconIconLayer = () => {
         isError: isTraconError
     } = useSelector((state: RootState) => state.matchedTracons);
 
+    const normalizeTracons = (
+        matchedTracons: MatchedTracon[],
+        fallbackTracons: FallbackTracon[]
+    ): TraconFeature[] => {
+        const matchedFeatures: TraconFeature[] = matchedTracons.map((tracon) => ({
+            uniqueId: tracon.traconInfo.uniqueId,
+            coordinates: tracon.traconInfo.coordinates,
+            iconId: `${imagePrefix}${tracon.traconInfo.uniqueId}`,
+            isFallback: false,
+            originalData: tracon
+        }));
+
+        const fallbackFeatures: TraconFeature[] = fallbackTracons.map((tracon) => ({
+            uniqueId: tracon.controllers[0].cid.toString(),
+            coordinates: tracon.edgeCoordinates,
+            iconId: `${imagePrefix}${tracon.controllers[0].cid}`,
+            isFallback: true,
+            originalData: tracon
+        }));
+
+        return [...matchedFeatures, ...fallbackFeatures];
+    };
+
+
+    const diffTracons = (newData: TraconFeature[], oldData: TraconFeature[]) => {
+        const toKeyedMap = (data: TraconFeature[]) =>
+            new Map(data.map(tracon => [tracon.uniqueId, tracon]));
+
+        const newMap = toKeyedMap(newData);
+        const oldMap = toKeyedMap(oldData);
+
+        const added: TraconFeature[] = [];
+        const updated: TraconFeature[] = [];
+        const removed: TraconFeature[] = [];
+
+        for (const [key, newTracon] of newMap) {
+            if (!oldMap.has(key)) {
+                added.push(newTracon);
+            } else {
+                const oldTracon = oldMap.get(key);
+                //only need to compare the controller callsign since icon is generated based on the callsign
+                if (newTracon.originalData.controllers[0].callsign !== oldTracon?.originalData.controllers[0].callsign) {
+                    updated.push(newTracon);
+                }
+            }
+        }
+
+        for (const [key, oldTracon] of oldMap) {
+            if (!newMap.has(key)) {
+                removed.push(oldTracon);
+            }
+        }
+
+        return {
+            added,
+            updated,
+            removed
+        };
+    };
+
+
     useEffect(() => {
         if (!mapRef?.getMap || isTraconLoading || isTraconError) return;
         const map = mapRef.getMap();
 
-        const addTraconIcons = () => {
-            if (matchedFallbackTracons && matchedFallbackTracons.length > 0) {
-                matchedFallbackTracons.forEach((tracon: FallbackTracon) => {
+        //combine both fallback and matchedTracon to a normalized structure
+        const combineData = normalizeTracons(matchedTracons, matchedFallbackTracons);
 
-                    const iconId = `${imagePrefix}${tracon.controllers[0].cid}`;
-                    const iconUrl = generateTraconIcon(tracon.controllers[0].callsign.slice(0, -4));
+        const {
+            added,
+            updated,
+            removed
+        } = diffTracons(combineData, previousTraconsRef.current);
 
-                    if (!map.hasImage(iconId)) {
-                        const image = new Image();
-                        image.onload = () => {
-                            if (!map.hasImage(iconId)) { // Check again before adding
-                                map.addImage(iconId, image, { sdf: false });
-                            }
-                        };
-                        image.onerror = () => {
-                            console.error(`Error loading icon for ${tracon.controllers[0].callsign}`);
-                        };
-                        image.src = iconUrl; // Set the Base64 data URL as the source
-                    }
-                });
-            }
 
-            if (matchedTracons && matchedTracons.length > 0) {
-                matchedTracons.forEach((tracon: MatchedTracon) => {
-                    const iconId = `${imagePrefix}${tracon.traconInfo.uniqueId}`;
-                    const iconUrl = generateTraconIcon(tracon.controllers[0].callsign.slice(0, -4));
+        console.log("Diff Tracon Added:", added.length);
+        console.log("Diff Tracon Updated:", updated.length);
+        console.log("Diff Tracon Removed:", removed.length);
 
-                    if (!map.hasImage(iconId)) {
-                        const image = new Image();
-                        image.onload = () => {
-                            if (!map.hasImage(iconId)) { // Check again before adding
-                                map.addImage(iconId, image, { sdf: false });
-                            }
-                        };
-                        image.onerror = () => {
-                            console.error(`Error loading icon for ${tracon.controllers[0].callsign}`);
-                        };
-                        image.src = iconUrl; // Set the Base64 data URL as the source
-                    }
-                });
-            }
-        };
+        [...added, ...updated].forEach((tracon) => {
+            const iconUrl = generateTraconIcon(
+                tracon.originalData.controllers[0].callsign.slice(0, -4)
+            );
 
-        addTraconIcons();
-
-        const onStyleData = () => {
-            addTraconIcons();
-        };
-
-        map.on("styledata", onStyleData);
-
-        return () => {
-            map.off("styledata", onStyleData);
-            try {
-                matchedTracons.forEach((tracon: MatchedTracon) => {
-                    const iconId = `${imagePrefix}${tracon.traconInfo.uniqueId}`;
-                    if (map.hasImage(iconId)) {
-                        map.removeImage(iconId);
-                    }
-                });
-
-                matchedFallbackTracons.forEach((tracon: FallbackTracon) => {
-                    const iconId = `${imagePrefix}${tracon.controllers[0].cid}`;
-                    if (map.hasImage(iconId)) {
-                        map.removeImage(iconId);
-                    }
-                });
-            } catch (e) {
-                console.error("Error cleaning up tracon icons:", e);
-            }
-        };
-
-    }, [matchedTracons, matchedFallbackTracons, isTraconLoading, isTraconError]);
-
-    const geoJsonData = useMemo(() => {
-        if (isTraconError ||
-                isTraconLoading ||
-                (matchedTracons.length <= 0 && matchedFallbackTracons.length <= 0)
-        ) {
-            return null;
-        }
-
-        let matchedTraconFeatures = [];
-        let matchedFallbackTraconFeatures = [];
-
-        if (matchedTracons && matchedTracons.length > 0) {
-            matchedTraconFeatures = matchedTracons.map((tracon: MatchedTracon) => {
-                const coordinates = [Number(tracon.traconInfo.coordinates[0]), Number(tracon.traconInfo.coordinates[1])];
-                return {
-                    type: "Feature",
-                    geometry: {
-                        type: "Point",
-                        coordinates,
-                    },
-                    properties: {
-                        ...tracon,
-                        uniqueId: tracon.traconInfo.uniqueId
-                    }
-                } as GeoJSON.Feature;
-            });
-        }
-
-        if (matchedFallbackTracons && matchedFallbackTracons.length > 0) {
-            matchedFallbackTraconFeatures = matchedFallbackTracons.map((tracon: FallbackTracon) => {
-                const name = (tracon.controllers && tracon.controllers.length !== 0) ?
-                    tracon.controllers[0].airport.name + " APP/DEP" : "-";
-                const cid = tracon.controllers[0].cid;
-                const traconProperties = {
-                    controllers: tracon.controllers,
-                    traconInfo: {
-                        name: name,
-                        coordinates: tracon.edgeCoordinates
+            if (!map.hasImage(tracon.iconId)) {
+                const image = new Image();
+                image.onload = () => {
+                    if (!map.hasImage(tracon.iconId)) {
+                        map.addImage(tracon.iconId, image, { sdf: false });
                     }
                 };
-                return {
-                    type: "Feature",
-                    geometry: {
-                        type: "Point",
-                        coordinates: tracon.edgeCoordinates
-                    },
-                    properties: {
-                        ...traconProperties,
-                        uniqueId: cid,
+                image.src = iconUrl;
+            }
+        });
 
-                    }
-                } as GeoJSON.Feature;
-            });
+        removed.forEach((tracon) => {
+            if (map.hasImage(tracon.iconId)) {
+                map.removeImage(tracon.iconId);
+            }
+        });
+
+
+        const newGeoJson: GeoJSON.FeatureCollection = {
+            type: "FeatureCollection",
+            features: combineData.map((tracon) => ({
+                type: "Feature",
+                geometry: {
+                    type: "Point",
+                    coordinates: tracon.coordinates
+                },
+                properties: {
+                    uniqueId: tracon.uniqueId,
+                    isFallback: tracon.isFallback,
+                    ...tracon.originalData
+                }
+            }))
+        };
+
+        const source: GeoJSONSource = map.getSource("tracon-icon-layer-source-globe") as GeoJSONSource;
+        if (source) {
+            source.setData(newGeoJson);
         }
 
+        previousTraconsRef.current = combineData;
 
-        return {
-            type: "FeatureCollection",
-            features: [...matchedTraconFeatures, ...matchedFallbackTraconFeatures],
-        } as GeoJSON;
+        return () => {
+            removed.forEach((tracon) => {
+                if (map.hasImage(tracon.iconId)) {
+                    map.removeImage(tracon.iconId);
+                }
+            });
+        };
+
 
     }, [matchedTracons, matchedFallbackTracons, isTraconLoading, isTraconError]);
-
-    if (!geoJsonData) return null;
 
 
     return (
         <Source
-            id="traon-icon-layer-source-globe"
+            id="tracon-icon-layer-source-globe"
             type="geojson"
-            data={geoJsonData}
+            // data={geoJsonData}
+            data={{
+                type: "FeatureCollection",
+                features: []
+            }}
         >
             <Layer
                 id="tracon-icon-globe-layer"
