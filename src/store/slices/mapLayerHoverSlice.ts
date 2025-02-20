73b513d2da1a7c678a4a115d1335792401a6397a
Zhengyang Zhao <mason.zh@gmail.com>
2025-02-19 23:56:09 -0600

    added mapLayerHoverSlice

---
+++ src/store/slices/mapLayerHoverSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+//TODO: TypeScript
+const mapLayerHoverSlice = createSlice({
+    name: "mapLayerHover",
+    initialState: {
+        hoveredTraffic: null,
+        hoveredController: null,
+        hoveredFir: null,
+        hoveredTracon: null
+    },
+    reducers: {
+        setHoveredTraffic(state, action) {
+            state.hoveredTraffic = action.payload;
+        },
+        setHoveredController(state, action) {
+            state.hoveredController = action.payload;
+        },
+        setHoveredTracon(state, action) {
+            state.hoveredTracon = action.payload;
+        },
+        setHoveredFir(state, action) {
+            state.hoveredFir = action.payload;
+        }
+    }
+});
+
+export const {
+    setHoveredTraffic,
+    setHoveredController,
+    setHoveredFir,
+    setHoveredTracon
+} = mapLayerHoverSlice.actions;
+export const mapLayerHoverReducer = mapLayerHoverSlice.reducer;
